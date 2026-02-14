9f5d4afdf778372789618ac9f7a4d1f6db4da414
gopaltamluk <125450667+gopaltamluk@users.noreply.github.com>
2026-02-14 08:28:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@ yes.addEventListener("click", () => {
   vid.style.display = "block";
   gif.src = "resources/idc.gif";// gif credit: https://tenor.com/view/peach-cat-kiss-animated-love-mwah-gif-25743978
   text.innerHTML = "Knew it babe 😘";
-  yes.innerHTML = '<a href=" ">Message me</a>';// inside the " " put your social media profile link
+  yes.innerHTML = "Message me";// inside the " " put your social media profile link
   yes.style.height = "90%";
   yes.style.width = "96%";
   no.style.display = "none";
@@ -66,6 +66,7 @@ yes.addEventListener("click", () => {
     vid.style.display = "none";
   }, 9000);
 });
+
 
 
 
